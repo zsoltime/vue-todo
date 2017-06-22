17f9f3e7e59d09a20daa12c9bd8c82c907cb4c27
Zsolt Meszaros <zsolt@sowad.com>
2017-06-22 19:00:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    todos: [{
+      id: Math.random(),
+      task: 'Learn Vue.js',
+      done: false,
+    }, {
+      id: Math.random(),
+      task: 'Learn Vuex',
+      done: false,
+    }, {
+      id: Math.random(),
+      task: 'Learn ES6',
+      done: true,
+    }, {
+      id: Math.random(),
+      task: 'Finish this todo app',
+      done: true,
+    }, {
+      id: Math.random(),
+      task: 'Make other awesome apps',
+      done: false,
+    }],
+  },
+  methods: {
+    beforeEnter: function(el) {
+      el.classList.add('todo__item--before-enter');
+    },
+    enter: function(el, done) {
+      // should only delay on page load
+      setTimeout(() => {
+        el.classList.add('todo__item--enter');
+        el.classList.remove('todo__item--before-enter');
+        done();
+      }, el.dataset.index * 100);
+    },
+    leave: function(el, done) {
+      el.classList.add('todo__item--leave');
+      el.classList.remove('todo__item--enter');
+      setTimeout(() => {
+        done();
+      }, 300);
+    },
+    removeTodo: function(id) {
+      this.todos = this.todos.filter(r => r.id !== id);
+    },
+  },
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Day 4 of My Vue.js Challenge
+
+This is the fourth day of my [Vue.js challenge](https://github.com/zsoltime/vue-basic-challenge), and I'm building a simple todo list app (because I'm so creative 😣).
+
+## User Stories
+
+- [x] I can see a list of todo items
+- [ ] I can add a todo item
+- [ ] I can edit a todo item
+- [ ] I can click on any of these items to mark as done
+- [x] I can delete any item
+- [ ] All new todo items I add are saved in my browser's local storage
